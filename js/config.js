bf07a2f0871813f21b507b7308b3b7dacc72017e
sanganagouda-k <33954107+sanganagouda-k@users.noreply.github.com>
2020-11-29 16:14:55 +0530

    new

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_Elx34Obaj',
+        userPoolClientId: '1esf3hidpmodq66k1cmhh687fv',
+        region: 'ap-south-1'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
